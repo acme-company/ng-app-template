52993cdf1de56872bf3da2dde2b1ef90462ae650
mike <mike@stuff.com>
2017-05-16 15:33:15 -0600

    deleted everything

--- app/main.aot.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowser }    from '@angular/platform-browser';
-import {AppModuleNgFactory} from './ngfactory/app/app.module.ngfactory';
-import { enableProdMode } from '@angular/core';
-
-console.log('Running in PROD mode');
-enableProdMode();
-
-console.log('Running AOT compiled');
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
