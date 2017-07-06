292cd8fc35b1f31d4705b9b7eb78967c3845ba06
pixelbits <pixelbits100@gmail.com>
2017-07-06 00:30:26 -0600

    updated

---
+++ e2e/tests/test.spec.ts
@@ -0,0 +1,12 @@
+import { browser, element, by, By, $, $$, ExpectedConditions } from 'protractor';
+
+describe("Label", () => {
+
+  it("should say Angular", () => {
+    browser.get('http://localhost:8080');    
+    let e = element(by.tagName('h1'));
+    expect(e.getText()).toEqual('Hello Angular');
+  });
+
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ gulp.task("webpack:build-dev", ['clean'], function(callback) {
 gulp.task("webpack-dev-server", function(callback) {
 	// modify some webpack config options
 	var config = Object.create(webpackConfig);
-	config.devtool = "eval";
+	config.devtool = "sourcemap";
 
 	// Start a webpack-dev-server
 	new WebpackDevServer(webpack(config), {
